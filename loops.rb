184ae1ec33c7d999c8a211709e918beef968dae8
Chris Bowen <chrisbowen@comcast.net>
2022-10-04 13:32:54 -0500

    created loops file

---
+++ loops.rb
@@ -0,0 +1,19 @@
+
+
+# puts "Mississippi"
+# puts "Mississippi"
+# puts "Mississippi"
+# puts "Mississippi"
+
+cities = ["Jackson", "Southhaven", "Oxford", "Vicksburg", "Booneville"]
+
+cities.each do |city|
+  puts "#{city}, Mississippi"
+end
+
+number = 0
+
+5.times do
+  number += 1
+  puts "#{number} Mississippi"
+end

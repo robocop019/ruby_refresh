17807f5c081b8a65f92dee65b74467c979f1033e
Chris Bowen <chrisbowen@comcast.net>
2022-10-04 13:08:13 -0500

    change store item from hash to class

--- store_item.rb
+++ store_item.rb
@@ -1,9 +1,43 @@
-item_1 = {name: 'red pepper', color: 'red', price: 1}
-item_2 = {:name => 'onion', :color => 'white', :price => 2}
-item_3 = {name: 'chips', color: 'yellow', price: 5}
+# item_1 = {name: 'red pepper', color: 'red', price: 1}
+# item_2 = {:name => 'onion', :color => 'white', :price => 2}
+# item_3 = {name: 'chips', color: 'yellow', price: 5}
 
-# p item_1
-# p item_2
-# p item_3
+# # p item_1
+# # p item_2
+# # p item_3
 
-puts "Item 1 is a #{item_1[:name]} and is #{item_1[:color]} with a price of #{item_1[:price]} dollar(s)."
+# puts "Item 1 is a #{item_1[:name]} and is #{item_1[:color]} with a price of #{item_1[:price]} dollar(s)."
+
+class Item
+
+  def initialize(name, color, price)
+    @name = name
+    @color = color
+    @price = price
+  end
+
+  def name
+    @name
+  end
+
+  def color
+    @color
+  end
+
+  def price
+    @price
+  end
+
+end
+
+item_1 = Item.new('Red pepper', 'Red', 1)
+item_2 = Item.new('Onion', 'White', 2)
+item_3 = Item.new('Potato Chips', 'Yellow', 5)
+
+p item_1
+p item_2
+p item_3
+
+puts "#{item_1.name} is the color #{item_1.color} and costs #{item_1.price} dollar(s)."
+puts "#{item_2.name} is the color #{item_2.color} and costs #{item_2.price} dollar(s)."
+puts "#{item_3.name} is the color #{item_3.color} and costs #{item_3.price} dollar(s)."

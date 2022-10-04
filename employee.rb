d916b4a06f985b0fa6126d520a418f9b6bb33a67
Chris Bowen <chrisbowen@comcast.net>
2022-10-04 12:26:59 -0500

    changed employee hash to class

--- employee.rb
+++ employee.rb
@@ -1,16 +1,40 @@
 
 
-employee_1 = ["Helen", "Bonhom Carter", 80000, true]
-employee_2 = ["Peter", "Gibbons", 60000, true]
+# employee_1 = ["Helen", "Bonhom Carter", 80000, true]
+# employee_2 = ["Peter", "Gibbons", 60000, true]
 
-puts employee_1[0] + ' ' + employee_1[1] + " makes " + employee_1[2].to_s + " a year."
+# puts employee_1[0] + ' ' + employee_1[1] + " makes " + employee_1[2].to_s + " a year."
 
-puts "#{employee_2[0]} #{employee_2[1]} makes #{employee_2[2]} a year."
+# puts "#{employee_2[0]} #{employee_2[1]} makes #{employee_2[2]} a year."
 
-employee_1 = {first_name: 'Helen', last_name: 'Bonhom Carter', salary: 80000, active: true}
-employee_2 = {:first_name => 'Peter', :last_name => 'Gibbons', :salary => 60000, :active => true}
+# employee_1 = {first_name: 'Helen', last_name: 'Bonhom Carter', salary: 80000, active: true}
+# employee_2 = {:first_name => 'Peter', :last_name => 'Gibbons', :salary => 60000, :active => true}
 
 
-puts "#{employee_1[:first_name]} #{employee_1[:last_name]} makes #{employee_1[:salary]} a year."
-puts "#{employee_2[:first_name]} #{employee_2[:last_name]} makes #{employee_2[:salary]} a year."
+# puts "#{employee_1[:first_name]} #{employee_1[:last_name]} makes #{employee_1[:salary]} a year."
+# puts "#{employee_2[:first_name]} #{employee_2[:last_name]} makes #{employee_2[:salary]} a year."
 
+
+
+class Employee
+
+  def initialize(first_name, last_name, salary, active)
+    @first_name = first_name
+    @last_name = last_name
+    @salary = salary
+    @active = active
+  end
+
+  def print_info
+    puts "#{@first_name} #{@last_name} makes #{@salary} a year."
+  end
+
+end
+
+employee_1 = Employee.new('Chris', 'Bowen', 35000, false)
+employee_2 = Employee.new('Ryan', 'Kuhns', 80000, true)
+
+
+p employee_1
+p employee_2
+employee_2.print_info

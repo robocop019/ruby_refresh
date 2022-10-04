2405854f836ad5167c10873a5a4f7ccc44ecf30f
Chris Bowen <chrisbowen@comcast.net>
2022-10-04 12:10:40 -0500

    created store item file

---
+++ store_item.rb
@@ -0,0 +1,9 @@
+item_1 = {name: 'red pepper', color: 'red', price: 1}
+item_2 = {:name => 'onion', :color => 'white', :price => 2}
+item_3 = {name: 'chips', color: 'yellow', price: 5}
+
+# p item_1
+# p item_2
+# p item_3
+
+puts "Item 1 is a #{item_1[:name]} and is #{item_1[:color]} with a price of #{item_1[:price]} dollar(s)."

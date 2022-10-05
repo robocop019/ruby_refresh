da9e2fc7008e9e7c7c5f72f828630f22286cc6ab
Chris Bowen <chrisbowen@comcast.net>
2022-10-05 11:11:38 -0500

    created inheritance example

--- employee.rb
+++ employee.rb
@@ -22,24 +22,6 @@ class Employee
 end
 
 class Manager < Employee
-  # attr_reader :first_name, :last_name, :salary, :active
-  # attr_writer :active
-
-  # def initialize(input_options)
-  #   @first_name = input_options[:first_name]
-  #   @last_name = input_options[:last_name]
-  #   @salary = input_options[:salary]
-  #   @active = input_options[:active]
-  # end
-
-
-  # def print_info
-  #   puts "#{first_name} #{last_name} makes #{salary} a year."
-  # end
-
-  # def give_annual_raise
-  #   @salary *= 1.05
-  # end
 
   def send_report
     puts "Sending email..."
